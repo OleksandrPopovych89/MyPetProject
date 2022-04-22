73e29b778348730a7358c8e8bafc950e7314fecb
Oleksndr <a.popovych.89@gmail.com>
2022-04-22 04:18:12 +0300

    New commit.

--- src/main/java/gui/table/MyTableModel.java
+++ src/main/java/gui/table/MyTableModel.java
@@ -59,6 +59,8 @@ public class MyTableModel extends AbstractTableModel {
         };
     }
 
+  
+
     public void delete(int[] index) {
         System.out.println(Arrays.toString(index));
         for (int i = index.length - 1; i >= 0; i--) {
